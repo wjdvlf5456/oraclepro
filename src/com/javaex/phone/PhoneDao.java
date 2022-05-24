32833616eb43d072e915485d9acdfd1aa2509db6
최정필 <wjdvlf1407@gmail.com>
2022-05-24 16:53:31 +0900

    드라이버 틀 짜는 중

--- src/com/javaex/phone/PhoneDao.java
+++ src/com/javaex/phone/PhoneDao.java
@@ -1,5 +1,61 @@
 package com.javaex.phone;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PhoneDao {
+
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@webdb_high?TNS_ADMIN=/Users/choijungphil/jungphil/Wallet_webdb";
+	private String id = "admin";
+	private String pw = "^^65Rhcemdtla";
+
+	// 생성자
+
+	// 메소드 - gs
+
+	// DB구축 메소드
+	public void getConnection() {
+		try {
+			// 1. 오라클 드라이버 로딩
+			Class.forName(driver);
+			// 2. 데이터베이스 접속
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		}
+
+	}
+
+	// 자원정리 메소드
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			System.out.println("erroe: " + e);
+		}
+	}
+
+	// 메소드 - 일반
 
 }
