8f77fefa459c4b8fbab6569a635a471250c176a0
최정필 <wjdvlf1407@gmail.com>
2022-05-25 10:15:34 +0900

    수정중

--- src/com/javaex/phone/PhoneApp.java
+++ src/com/javaex/phone/PhoneApp.java
@@ -1,16 +1,6 @@
 package com.javaex.phone;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.util.List;
 import java.util.Scanner;
 
@@ -29,7 +19,6 @@ public class PhoneApp {
 
 		while (true) {
 
-			int i = 1;
 			System.out.println("");
 			System.out.println("1.리스트  2.등록  3.삭제  4.검색  5.종료");
 			System.out.println("-------------------------------------------");
@@ -37,8 +26,8 @@ public class PhoneApp {
 			int contents = sc.nextInt();
 			int active = contents;
 
-			// ---------5.종료------------
-			if (active == 5) {
+			// ---------6.종료------------
+			if (active == 6) {
 				System.out.println("*******************************************");
 				System.out.println("*                감사합니다               *");
 				System.out.println("*******************************************");
@@ -62,67 +51,57 @@ public class PhoneApp {
 				String company = sc.nextLine();
 				System.out.println("[등록되었습니다.]");
 
-				PersonVo person = new PersonVo(name, hp, company);
+				PersonVo vo = new PersonVo(name, hp, company);
+				phoneDao.personinsert(vo);
+				
+				// ------------ 3.수정 ------------
+				System.out.println("<3.등록>");
+				System.out.print(">이름: ");
+				String upName = sc.nextLine();
+				
+				String upHp = sc.nextLine();
+				System.out.print(">휴대전화: ");
+				
+				System.out.print(">회사전화: ");
+				String upCompany = sc.nextLine();
+				System.out.println("[등록되었습니다.]");
+				
+				PersonVo upVo = new PersonVo(upName, upHp, upCompany);
+				phoneDao.personinsert(upVo);
+				
 
-				// ------------ 3.삭제 ------------
-			} else if (active == 3) {
-				System.out.println("<3.삭제>");
+				// ------------ 4.삭제 ------------
+			} else if (active == 4) {
+				System.out.println("<4.삭제>");
 				System.out.print(">번호: ");
 				int delete = sc.nextInt();
-				int deleteNumber = delete - 1;
+				phoneDao.persondelete(delete);
 
-				// ------------ 4.검색 ------------
-			} else if (active == 4) {
-				System.out.println("<4.검색>");
+				// ------------ 5.검색 ------------
+			} else if (active == 5) {
+				System.out.println("<5.검색>");
 				System.out.print(">이름: ");
 				String search = sc.nextLine();
 				char cSearch = search.charAt(0);
 
-				for (PersonVo person : search) {
-					String pName = person.getName();
-					char cn1 = pName.charAt(0);		// 이
-					char cn2 = pName.charAt(1);
-					char cn3 = pName.charAt(2);
-					if (cSearch == cn1 || cSearch == cn2 || cSearch == cn3) {
-						System.out.print(i);
-						// person.showList();
-					}
-					i++;
-				}
-
 				// ------------ 재입력 ------------
 			} else {
 				System.out.println("[다시 입력해 주세요]");
-				change(fList);
 			}
+			sc.close();
 		}
-		for (
-
-		PersonVo person : fList) {
-			String savestr = person.getName() + "," + person.getHp() + "," + person.getCompany();
-
-		}
-		sc.close();
 
 	}
 
 	// 실시간으로 반영하기 위한 전역변수
 	public static void change(List<PersonVo> fList) throws IOException {
-		OutputStream os = new FileOutputStream("./PhoneDB.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
+		Scanner sc = new Scanner(System.in);
 
 		for (PersonVo person : fList) {
 			String savestr = person.getName() + "," + person.getHp() + "," + person.getCompany();
 
-			bw.write(savestr);
-			bw.newLine();
-
 		}
-
-		bw.flush();
-		bw.close();
-
+		sc.close();
 	}
 
 }
